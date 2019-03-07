52f520b236052d73d27e0d7fc14ca1d30ae815d8
fred.vunk <fred.vunk@khk.ee>
2019-03-07 09:08:41 +0200

    Refactoring add()

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 function add(x, y) {
-    return x + y;
+    var result;
+    if (typeof x && typeof y) !== 'number') {
+    throw new Error('Params must be a number.');
+    }
+    result = x +y;
+if parseInt(result) !== result) {
+    result = parseFloat(result.toFixed(1));
+}
+    return result;
 }
 
 add(2, 3);
@@ -8,5 +16,6 @@ add(2, 3);
 add('2', '3');
 
 // Expect 2+3 = 5
-// Expect error IF non-numbers are used
-// Expect 0.1 + 0.2 = 0.30000
+// Expect error IF non-numbers are used | Now Passed
+// Expect 0.1 + 0.2 = 0.30000 | Now passed
+
