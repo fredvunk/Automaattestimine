9f3e3f25c9c8627d4c72e0548998a8bc4724fafa
fred.vunk <fred.vunk@khk.ee>
2019-03-07 09:27:43 +0200

    Writing a test

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-// Example
-// Expect add(2,3) to equal 5
-expect(add(2,3)).tobe(5);
+expect(add(0.1, 0.2)).toBe(0.3);
 
-//Example:
-// Expect add() to throw an error if x/y are not numbers
-Expect(add(2, 'test')).toThrow();
-
-//test
-// Expect add(0.1, 0.2) to equal 0.3
-
-Expect (add(0.1, 0.2)).tobe(0.3);
+expect(add(0.1,0.2)).not.toBe(0.1);
