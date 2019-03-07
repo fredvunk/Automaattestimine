983302172193937872eb4033ff49909f7b78ace0
fred.vunk <fred.vunk@khk.ee>
2019-03-07 09:00:15 +0200

    Defining Expectations

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-function add (x, y) {
-    return x+ y;
+function add(x, y) {
+    return x + y;
 }
 
 add(2, 3);
 add(2, 3);
 
-add('2','3');
+add('2', '3');
+
+// Expect 2+3 = 5
+// Expect error IF non-numbers are used
+// Expect 0.1 + 0.2 = 0.30000
